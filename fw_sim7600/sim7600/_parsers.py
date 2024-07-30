e6c58698ef31b51c8172b771b84e81fa3883b602
Roberto Pompermaier <robypomper@gmail.com>
2024-07-30 15:16:29 +0200

    feat(Props): Updated `pos_gps_lat_degrees`, `pos_gps_lon_degrees`, `pos_gnss_lat_degrees` and `pos_gnss_lon_degrees` parser
    pos_gps_lat_degrees
    pos_gps_lon_degrees
    pos_gnss_lat_degrees
    pos_gnss_lon_degrees
    - parsers: added props_parser_gpsinfo_coordinates

--- fw_sim7600/sim7600/_parsers.py
+++ fw_sim7600/sim7600/_parsers.py
@@ -1,3 +1,28 @@
 #!/usr/bin/python3
 
-# N/A
+def props_parser_gpsinfo_coordinates(raw_value: str) -> int:
+    try:
+        return convert_to_decimal_degrees(raw_value)
+
+    except Exception:
+        return 0
+
+
+# https://www.earthpoint.us/convert.aspx
+def convert_to_decimal_degrees(raw_value: str) -> float:
+    """ Convert a string in `dddmm.mmmmmm` format to decimal degrees """
+    if len(raw_value) < 7:  # Ensure the string is long enough
+        raise ValueError("Invalid format for degrees")
+    point_index = raw_value.index('.')
+    degrees = int(raw_value[:point_index-2])
+    minutes = float(raw_value[point_index-2:])
+    decimal_degrees = degrees + minutes / 60
+    return decimal_degrees
+
+
+def convert_to_degrees_minutes_seconds(decimal_degrees: float) -> tuple:
+    degrees = int(decimal_degrees)
+    minutes_decimal = abs(decimal_degrees - degrees) * 60
+    minutes = int(minutes_decimal)
+    seconds = (minutes_decimal - minutes) * 60
+    return degrees, minutes, seconds
