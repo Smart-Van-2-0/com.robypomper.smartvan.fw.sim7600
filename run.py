eed80c53c1233f314a9ea175b5412fb98f9281ba
Roberto Pompermaier <robypomper@gmail.com>
2024-08-03 17:18:19 +0200

    refactor(Run): Added `development` params to principal methods to handle exceptions and print the stracktrace only if in dev mode

--- run.py
+++ run.py
@@ -217,7 +217,7 @@ def _publish_dbus_object(dbus, dbus_obj):
         logger.warning("Received terminate signal during Object publication on DBus, exit.")
 
 
-def _main_loop(dev, dbus_obj):
+def _main_loop(dev, dbus_obj, development=False):
     """ Current script's main loop. """
 
     global must_shutdown
@@ -238,15 +238,16 @@ def _main_loop(dev, dbus_obj):
                 logger.warning("No data read, nothing to update")
             else:
                 for property_code in dev.latest_data:
-                    _process_property(dev, dbus_obj, property_code)
+                    _process_property(dev, dbus_obj, property_code, development)
 
         except KeyboardInterrupt:
             logger.info("Terminating required by the user.")
             must_shutdown = True
         except Exception as unknown_error:
-            logger.error("Unknown error on Main Loop: {}".format(unknown_error))
-            import traceback
-            traceback.print_exc()
+            logger.error("Unknown error on Main Loop: {}, retry later".format(unknown_error))
+            if development is True:
+                import traceback
+                traceback.print_exc()
 
         logger.debug("End fetch/pull device")
 
@@ -264,7 +265,7 @@ def _main_loop(dev, dbus_obj):
     logger.info(FW_NAME + " Main Loop terminated.")
 
 
-def _process_property(dev, dbus_obj, property_code):
+def _process_property(dev, dbus_obj, property_code, development=False):
     """
     Get and parse the Device's property and, if it used to elaborate a
     calculated value, the calculated value will be refreshed.
@@ -275,7 +276,7 @@ def _process_property(dev, dbus_obj, property_code):
         property_name = PROPS_CODES[property_code]['name']
         property_parser = PROPS_CODES[property_code]['parser']
     except KeyError:
-        logger.warning("Read unknown property code '{}' <{}>, skipped.".format(property_code, property_value_raw))
+        logger.warning("Read unknown property code '{}' <raw value: {}>, skipped.".format(property_code, property_value_raw))
         return
 
     try:
@@ -291,24 +292,34 @@ def _process_property(dev, dbus_obj, property_code):
         }
         dbus_obj.update_property(property_name, property_value)
         logger.info("R ==> {:<16} = '{}'".format(property_name, str(property_value)))
-        _update_property_derivatives(dbus_obj, property_name)
+        _update_property_derivatives(dbus_obj, property_name, development)
 
     except ValueError:
         logger.warning("Property '{}' <{}> raw value malformed, skipped.".format(property_name, property_value_raw))
+        if development is True:
+            import traceback
+            traceback.print_exc()
     except TypeError:
         logger.warning("DBus property '{}' <{}> malformed, skipped.".format(property_name, property_value_raw))
+        if development is True:
+            import traceback
+            traceback.print_exc()
     except KeyError:
         logger.warning("Property '{}' not used by current DBus object definition, skipped.".format(property_name))
+        if development is True:
+            import traceback
+            traceback.print_exc()
     except KeyboardInterrupt as err:
         raise err
     except Exception as err:
-        logger.warning("Unknown error on parsing and updating property '{}': [{}] {}"
+        logger.warning("Unknown error on parsing and updating property '{}': [raw value: {}] {}"
                        .format(property_name, type(err), str(err)))
-        import traceback
-        traceback.print_exc()
+        if development is True:
+            import traceback
+            traceback.print_exc()
 
 
-def _update_property_derivatives(dbus_obj, property_name):
+def _update_property_derivatives(dbus_obj, property_name, development=False):
     """ Get and parse the Device's property and notify his update on DBus. """
 
     for c_property_name in CALC_PROPS_CODES:
@@ -340,11 +351,9 @@ def _update_property_derivatives(dbus_obj, property_name):
 
         except Exception as err:
             logger.warning("Error calculating '{}' property: {}".format(c_property_name, err))
-            # uncomment for calculators development
-            # print("###################################")
-            # import traceback
-            # traceback.print_exc()
-            # print("###################################")
+            if development is True:
+                import traceback
+                traceback.print_exc()
 
 
 def _register_kill_signals():
@@ -363,7 +372,7 @@ def __handle_kill_signals(signo, _stack_frame):
     must_shutdown = True
 
 
-def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=False):
+def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=False, development=False):
     """ Initialize a Device to read data and a DBus Object to share collected data. """
     global dev_global
 
@@ -374,10 +383,14 @@ def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=Fa
         dev_global = _init_device(port, speed, True, simulate_dev)
         if not dev_global.is_connected and must_shutdown:
             exit(0)
+        if dev_global.device_type_code == "":
+            logger.warning("Device not recognized, exit.")
+            exit(-1)
     except Exception as err:
         logger.warning("Error on initializing Device: " + str(err))
-        import traceback
-        traceback.print_exc()
+        if development is True:
+            import traceback
+            traceback.print_exc()
         exit(-1)
 
     # Init DBus Object
@@ -387,8 +400,9 @@ def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=Fa
         dbus_obj = _init_dbus_object(dbus_name, dev_id, obj_path, dbus_iface)
     except Exception as err:
         logger.warning("Error on initializing DBus Object: " + str(err))
-        import traceback
-        traceback.print_exc()
+        if development is True:
+            import traceback
+            traceback.print_exc()
         exit(-1)
 
     # Publish on init DBus
@@ -417,7 +431,7 @@ def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=Fa
         exit(-1)
 
     try:
-        _main_loop(dev_global, dbus_obj)
+        _main_loop(dev_global, dbus_obj, development)
     except Exception as err:
         logger.warning("Error on main thread: " + str(err))
         exit(-1)
@@ -443,5 +457,5 @@ if __name__ == '__main__':
         args.quiet = False
 
     logger = _init_logging(args.dev, args.debug, args.quiet)
-    main(args.port, args.speed, args.dbus_name, args.dbus_obj_path, args.dbus_iface, args.simulate)
+    main(args.port, args.speed, args.dbus_name, args.dbus_obj_path, args.dbus_iface, args.simulate, args.dev)
     exit(EXIT_SUCCESS)
